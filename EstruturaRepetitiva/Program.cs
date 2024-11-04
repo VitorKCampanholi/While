8d1ba3eec43a1fa6feee75202a8e48d84cd649cc
VKBOOY <VKBOOY@DESKTOP-DE9CVF9>
2024-11-04 15:27:27 -0300

    Adicionar arquivos de projeto.

---
+++ EstruturaRepetitiva/Program.cs
@@ -0,0 +1,18 @@
+﻿using System.Globalization;
+
+
+    Console.Write("Digite um numero: ");
+    double x = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+    while (x >= 0.0) {
+
+    double raiz = Math.Sqrt(x);
+ 
+    Console.WriteLine(raiz.ToString("F3", CultureInfo.InvariantCulture));
+    Console.Write("Digite outro Número : ");
+    x = double.Parse(Console.ReadLine(),CultureInfo.InvariantCulture);
+
+
+}
+    Console.WriteLine("Numero negativo");
+    
